b84a79c8fe71c66b763b9903f059a9bd2cb85b6f
chmcs2026vipulmishra-web <chmcs2026.vipul.mishra@gmail.com>
2026-02-21 12:32:41 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    // baseURL: 'http://localhost:3000'
+    baseURL:'https://bookstore-uu6w.onrender.com'
 })
 export default api;
